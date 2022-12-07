bb1224acaeed6915c30d85868e782e5894c0326e
Nali1919 <my@email.com>
2022-12-07 19:09:57 +0100

    Eseguo migration menu

--- database/migrations/2022_12_07_153404_create_menus_table.php
+++ database/migrations/2022_12_07_153404_create_menus_table.php
@@ -15,6 +15,12 @@ class CreateMenusTable extends Migration
     {
         Schema::create('menus', function (Blueprint $table) {
             $table->id();
+            $table->string('name')->nullable(false);
+            $table->text('description')->nullable(false);
+            $table->string('slug')->unique();
+            $table->string('image')->nullable(true);
+            $table->float('price', 7,2)->nullable(false);
+            $table->boolean('visibility')->default(false);
             $table->timestamps();
         });
     }
