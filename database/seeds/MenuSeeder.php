13721503f4416dc7d78d5953e57ae88eefd13f61
Nali1919 <my@email.com>
2022-12-07 19:12:43 +0100

    Impostazione inziale MenuSeeder

--- database/seeds/MenuSeeder.php
+++ database/seeds/MenuSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Menu;
+require 'App/Utilities/slug.php';
 
 class MenuSeeder extends Seeder
 {
@@ -12,5 +14,11 @@ class MenuSeeder extends Seeder
     public function run()
     {
         //
+        for($i = 0; $i<=10; $i++){
+            $menu = new Menu();
+            $menu->name = 'Il vecchio frantoio' . $i;
+
+        }
+
     }
 }
