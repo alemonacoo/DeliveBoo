a7522aae0bcb2895ec16137b203de7b7abcf5fda
Nali1919 <my@email.com>
2022-12-07 20:25:28 +0100

    Creazione ed invio MenuSeeder

--- database/seeds/MenuSeeder.php
+++ database/seeds/MenuSeeder.php
@@ -14,9 +14,34 @@ class MenuSeeder extends Seeder
     public function run()
     {
         //
-        for($i = 0; $i<=10; $i++){
+$names1 = ["Spaghetti allo scoglio", "Pasta alla carbonara", "Salmon Salad", "Chicken Salad", "Acqua", "Coca cola", "Birra artigianale", "Pennete panna e salmone", "Taco Salad", "Pasta Salad"];
+
+
+
+
+        $ingred1 = [
+            "Spaghetti, gamberi, cozze, arsele, pomodoro",
+            "Spaghetti, guanciale, uova, pecorino, sale, pepe",
+            "Salmone marinato, Menta, Avocado, Mango, Edamame, Germogli di soia",
+            "Pollo, pomodori, mais, mozzarella",
+            "Acqua",
+            "Acqua, Anidride carbonica, Zucchero, Aromi naturali, Caffeina",
+            "Malto d'orzo, luppolo, lievito, acqua",
+            "Pennete, panna, salmone, erba cipollina",
+            "Carne lessa, pomodori ciliegini, lattuga romana, avocado",
+            "Fusilli, pomodori, peperoni rossi, pomodori, basilico, olio extra vergine d'oliva",
+        ];
+
+
+
+        for($i = 0; $i<10; $i++){
             $menu = new Menu();
-            $menu->name = 'Il vecchio frantoio' . $i;
+            $menu->name = $names1[$i];
+            $menu->description = $ingred1[$i];
+            $menu->price = number_format(rand(100, 1000) / 100, 2);
+            $menu->slug = getSlugForTable($menu->name, 'menus');
+            $menu->save();
+
 
         }
 
