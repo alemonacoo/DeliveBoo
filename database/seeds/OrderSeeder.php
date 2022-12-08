822c8bff9a2b1b76358db99f580c021dabcfba98
TreleaniFederico <treleani.federico@gmail.com>
2022-12-08 13:44:22 +0100

    add order migration + seeder

---
+++ database/seeds/OrderSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Order;
+
+require 'App/Utilities/slug.php';
+
+
+class OrderSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        for ($i = 1; $i <= 10; $i++) {
+            $order = new Order();
+            $order->total = rand(100, 1000) / 10;
+            $order->address = 'Via dei Cento n.' . $i;
+            $order->date_time = date('Y-m-d H:i:s');
+            $order->save();
+        }
+    }
+}
