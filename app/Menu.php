740c656dff9160a0be2d9552b6ef6f36c1f2b442
alemonacoo <ale.monaco@outlook.com>
2022-12-09 12:39:58 +0100

    fixing menu model

--- app/Menu.php
+++ app/Menu.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Menu extends Model
 {
     //
-     protected $fillable = ['name', 'description', 'slug', 'price', 'image', 'visibility', 'restaurant_id'];
+     protected $fillable = ['name', 'description', 'slug', 'price', 'image', 'visibility', 'restaurant_slug'];
 
     public function restaurant(){
         return $this->belongsTo('App\Restaurant');
