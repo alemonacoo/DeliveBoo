1172100f4f0bd302ca914b08744d5972996cb88c
alemonacoo <ale.monaco@outlook.com>
2022-12-12 12:07:54 +0100

    Adding categories seeder

--- database/seeds/CategorySeeder.php
+++ database/seeds/CategorySeeder.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Category;
 use Illuminate\Database\Seeder;
+require 'App/Utilities/slug.php';
+
 
 class CategorySeeder extends Seeder
 {
@@ -12,5 +15,14 @@ class CategorySeeder extends Seeder
     public function run()
     {
         //
+        $categories = ['Vegetariano', 'Messicano', 'Cinese', 'Indiano', 'Greco', 'Giapponese', 'Italiano', 'Thai', 'Pizza', 'Americano'];
+
+        for($i = 0; $i < count($categories); $i++){
+            $category = new Category();
+            $category->name = $categories[$i];
+            $category->slug = getSlugForTable($category->name, 'categories');
+            $category->save();
+        }
+
     }
 }
