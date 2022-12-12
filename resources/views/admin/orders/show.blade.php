47eb3092031f0bdf4dde51f447e43abc979f5ee5
TreleaniFederico <treleani.federico@gmail.com>
2022-12-12 16:12:26 +0100

    remove delete form

--- resources/views/admin/orders/show.blade.php
+++ resources/views/admin/orders/show.blade.php
@@ -5,11 +5,4 @@
     <h1>Ordine numero: {{ $order->id }}</h1>
     <p>Effettuato il: {{ $order->created_at->format('m-d') }} alle {{ $order->created_at->format('H:i') }}</p>
     <p>Indirizzo consegna: {{ $order->address }}</p>
-
-    {{-- delete --}}
-    <form action="{{ route('admin.orders.destroy', $order->id) }}" method="POST">
-        @csrf
-        @method('DELETE')
-        <input type="submit" value="Elimina Ordine" onclick="return confirm('Confermi di voler eliminare l\'ordine?')">
-    </form>
 @endsection
