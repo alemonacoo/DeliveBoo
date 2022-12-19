a8a55fa41eeace66329f13ebe740d5677e08cd31
TreleaniFederico <treleani.federico@gmail.com>
2022-12-19 01:12:10 +0100

    add front routes

--- resources/js/router.js
+++ resources/js/router.js
@@ -3,7 +3,8 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 
 import Home from './pages/Home.vue';
-import CategoryRestaurants from './pages/CategoryRestaurants.vue';
+import RestaurantsList from './pages/RestaurantsList.vue';
+import RestaurantMenu from './pages/RestaurantMenu.vue';
 import NotFound from './pages/NotFound.vue';
 
 Vue.use(VueRouter);
@@ -11,8 +12,9 @@ Vue.use(VueRouter);
 const router = new VueRouter({
     routes: [
         { path: '/', name: 'home', component: Home },
-        { path: '/categories/:slug', name: ':slug-restaurants', component: CategoryRestaurants },
-        { path: '/*', name: 'not-found', component: NotFound }
+        { path: '/category/:id', name: 'restaurants-list', component: RestaurantsList },
+        { path: '/restaurant/:slug', name: 'restaurant-menu', component: RestaurantMenu },
+        { path: "/*", name: "NotFound", component: NotFound }
     ]
 });
 
