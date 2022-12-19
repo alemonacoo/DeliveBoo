adf4c436e534e3700b245f252652b18649128f90
TreleaniFederico <treleani.federico@gmail.com>
2022-12-19 16:35:24 +0100

    add restaurant name

--- resources/js/pages/RestaurantMenu.vue
+++ resources/js/pages/RestaurantMenu.vue
@@ -2,14 +2,8 @@
     <div class="tt">
 
         <div class="mb-5 hh">
-            <h1>Ristorante 1</h1>
-
-            <p>Corso Vittorio II, Milano 86704</p>
-
+            <h1>{{ this.$route.params.slug }}</h1>
             <hr>
-
-
-
         </div>
 
         <h2>Il nostro Menu</h2>
@@ -18,7 +12,7 @@
                 <h2>{{ item.name }}</h2>
                 <p>{{ item.description }}</p>
                 <p @click="() => onSelect(item)"> Da
-                {{ item.price }} $</p>
+                    {{ item.price }} $</p>
 
             </div>
 
