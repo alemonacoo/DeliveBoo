76f0c179a5717b87e10c2ce5181d8ea5d5610cc7
TreleaniFederico <treleani.federico@gmail.com>
2022-12-20 18:25:59 +0100

    add OrderComplete page + route

---
+++ resources/js/pages/OrderComplete.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-6">
+                <div class="card" style="width: 18rem;">
+                    <div class="card-body">
+                        <h5 class="card-title text-center">Ordine Completato</h5>
+                        <h6 class="card-subtitle mb-2 text-muted text-center">Il Bello Comincia Ora</h6>
+                        <a href="/" class="card-link">Torna alla Home</a>
+                        <a href="" class="card-link">Torna al Menù</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'OrderComplete'
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
