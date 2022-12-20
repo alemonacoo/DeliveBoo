ae106f54607b53aec70d8df884140b442b485eee
davishka-hikkaduwa <sanjaya.hikkaduwa@outlook.com>
2022-12-20 21:48:49 +0100

    npm i

--- public/js/back.js
+++ public/js/back.js
@@ -49495,8 +49495,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/alessandromonaco/Desktop/Boolean/Progetto/DeliveBoo/resources/js/back.js */"./resources/js/back.js");
-module.exports = __webpack_require__(/*! /Users/alessandromonaco/Desktop/Boolean/Progetto/DeliveBoo/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/davishkahikkaduwa/Desktop/boolean/PHP/DeliveBoo/resources/js/back.js */"./resources/js/back.js");
+module.exports = __webpack_require__(/*! /Users/davishkahikkaduwa/Desktop/boolean/PHP/DeliveBoo/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
