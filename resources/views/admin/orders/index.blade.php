50311d23070e2f3b2323800a8436086a744ef047
alemonacoo <ale.monaco@outlook.com>
2022-12-21 11:06:19 +0100

    fixing orders grid

--- resources/views/admin/orders/index.blade.php
+++ resources/views/admin/orders/index.blade.php
@@ -5,6 +5,7 @@
     <div class="orders-container">
         <div class="orders-columns orange">
             <p>ID</p>
+            <p>Indirizzo</p>
             <p>Piatti</p>
             <p>Totale</p>
         </div>
@@ -12,10 +13,11 @@
             @foreach ($orders as $order)
             <div class="orders-columns">
                 <p>{{ $order->id }}</p>
+                <p>{{$order->address}}</p>
                 <div>
                     @foreach ($order->menu as $item)
                     <p>
-                        {{ $item->name }} - {{ $item->price }}$
+                        {{ $item->name }}
                     </p>
                     @endforeach
                 </div>
